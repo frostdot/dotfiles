e8cd6a1faf01fc2a2bfdf7a8236408e2e095f66a
Makoto Takami <frostdot@gmail.com>
2015-01-26 10:56:09 +0900

    add wgetrc to init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.cshrc ~/.cshrc
 ln -sf ~/.dotfiles/.screenrc ~/.screenrc
+ln -sf ~/.dotfiles/.wgetrc ~/.wgetrc
 ln -sf ~/.dotfiles/.vim ~/.vim
 
 git submodule init
